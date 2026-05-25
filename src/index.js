cb37c35c2d84487894d2fe5230b73955a9500301
Jess90AB <82506428+Jess90AB@users.noreply.github.com>
2026-05-25 18:19:48 +0800

    Added API functionality to search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let CityElement = document.querySelector("#city");
+
+  CityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "465af09aod8c2624e9ft9e3093eb0ebb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let CityElement = document.querySelector("#city");
-  CityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.getElementById("search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kyiv");
